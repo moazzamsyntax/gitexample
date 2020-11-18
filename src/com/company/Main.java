7a8489be6f75e963d8420ec94596ed45d645634f
moazzamsyntax <moazzamsyntax@gmail.com>
2020-11-17 22:52:55 -0500

    2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,11 @@ public class Main {
     public static void main(String[] args) {
 
     Car audi= new Car();
-    
+    audi.doors=4;
+    audi.color="red";
+    audi.model="a6";
+    audi.number="34436";
+    audi.engine="50000cc";
 
 
 
